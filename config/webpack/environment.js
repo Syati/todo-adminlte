b9a648558f2f857b13d6b65f0968511a774d52ed
Mizuki Yamamoto <m.yamamoto@raksul.com>
2018-04-30 23:24:50 +0700

    Initial admin-lte settings
    - remove assets

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,22 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery'
+  })
+);
+
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1
+  }
+});
 
 module.exports = environment
