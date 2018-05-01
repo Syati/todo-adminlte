f0675e23c34bd4fd7852f2d8c5ddf2999e589901
Mizuki Yamamoto <m.yamamoto@raksul.com>
2018-05-01 17:20:14 +0700

    Display todos on index page

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,11 @@ Bundler.require(*Rails.groups)
 module TodoAdminlte
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.default_locale = :ja
     config.load_defaults 5.2
+
+    # load_path for International (I18n)
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
