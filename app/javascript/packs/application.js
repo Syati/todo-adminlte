4759752c3d4ef45cfcad2941c48d7ddfa6c7cb75
Mizuki Yamamoto <m.yamamoto@raksul.com>
2018-05-01 23:15:31 +0700

    Use turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,16 @@
 import 'src/application'
 
 // lib js
-import 'admin-lte'
+import 'admin-lte/dist/js/adminlte'
+import Turbolinks from 'turbolinks'
 
 // images
 require.context('images', true, /\.(png|jpg|jpeg|svg)$/)
+
+
+Turbolinks.start();
+
+document.addEventListener('turbolinks:load', () => {
+  //it's for tree view js on side menu
+  window.dispatchEvent(new Event('load'))
+})
