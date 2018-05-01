2b4b08ea81f95f7e435c4d2785ec0ba9adb913e0
Mizuki Yamamoto <m.yamamoto@raksul.com>
2018-05-01 14:47:12 +0700

    Add seedbank

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'slim-rails'
 
+gem 'seedbank'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ db/seeds/development/todo.seeds.rb
@@ -0,0 +1,6 @@
+Todo.find_or_create_by(id:1, title: 'todo1', desc: 'desc1')
+Todo.find_or_create_by(id:2, title: 'todo2', desc: 'desc2')
+Todo.find_or_create_by(id:3, title: 'todo3', desc: 'desc3')
+Todo.find_or_create_by(id:4, title: 'todo4', desc: 'desc4')
+Todo.find_or_create_by(id:5, title: 'todo5', desc: 'desc5')
+
