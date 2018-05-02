fb7f60f6be0357c3aef01d89f949e9fc52a60818
Mizuki Yamamoto <m.yamamoto@raksul.com>
2018-05-02 23:31:58 +0700

    Set up js lib vue initially
    - vue-loader 15 doesn't work correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-   def active_class(link_path)
-     link_path.include?(url_for(controller: @_controller.controller_name, action: :index)) ? "active" : ""
-   end
+  def active_class(link_path)
+    begin
+      link_path.include?(url_for(controller: @_controller.controller_name, action: :index)) ? "active" : ""
+    rescue
+      ''
+    end
+  end
 end
