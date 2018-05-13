fe237586d24ba7e2a7f9e5318fe97afaa28363a6
Mizuki Yamamoto <m.yamamoto@raksul.com>
2018-05-13 16:26:42 +0700

    Add annotate on models

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string(255)
+#  desc       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Todo < ApplicationRecord
 end
