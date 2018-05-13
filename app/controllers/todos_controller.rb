098d7395eb00712463c11b3810bf78554dc8dd44
Mizuki Yamamoto <m.yamamoto@raksul.com>
2018-05-13 21:59:15 +0700

    Add flash message

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,12 +1,29 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: %i[edit update]
+
   def index
     @todos = Todo.all
   end
 
   def edit
-    @todo = Todo.find(params[:id])
   end
 
   def update
+    if @todo.update(todo_params)
+      flash[:success] = t('flashes.updated')
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :desc)
   end
 end
